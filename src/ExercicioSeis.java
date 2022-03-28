b0e7dc464569dae9dcf75560923ff23db460428d
Matheus-Joao <Matheus@gmail.com-Joao@gmail.com>
2022-03-28 09:41:47 -0300

    desenvolvimento do exercicio seis

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,15 @@
+
+public class ExercicioSeis {	
+	public static void main(String[] args) {
+		double peso = 205050;
+		if (peso < 4) {
+			System.out.println("Abaixo do peso");
+		}
+		else if (peso > 10) {
+			System.out.println("Acima do peso");
+		}
+		else {
+			System.out.println("peso normal");
+		}
+	}
+}
