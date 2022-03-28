3d3fb8ab18bb6805839b02cc7ca1452dd321e934
Matheus-Joao <Matheus@gmail.com-Joao@gmail.com>
2022-03-28 10:24:27 -0300

    desenvolvimento do exercicio oito

---
+++ src/ExercicioOito.java
@@ -0,0 +1,13 @@
+
+public class ExercicioOito {
+	public static void main(String[] args) {
+		String diaSemana = "Domingo";
+		
+		if(diaSemana != "sabado" && diaSemana != "domingo") {
+			System.out.println("Você pode ir ao banco.");
+		}
+		else {
+			System.out.println("O banco está fechado, tente outro dia.");
+		}
+	}
+}
