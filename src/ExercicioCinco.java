4157427cbaa90c4e9adad88577cd3fe75a91f427
Matheus-Joao <Matheus@gmail.com-Joao@gmail.com>
2022-03-28 09:35:46 -0300

    desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,14 @@
+
+public class ExercicioCinco 
+{
+	public static void main(String[] args) {
+	
+		double trabalhoDeHistoria = 8.0;
+		double trabalhoDeMatematica = 7.0;
+		double trabalhoDeCiencia = 10;
+		double trabalhoDeGeografia = 9.5;
+		double mediaAluno = (trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica) / 4;
+		
+		System.out.println("A média do aluno é: " + mediaAluno);
+}
+}
